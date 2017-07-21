354f4db8c8b3c3ea8386d8607f89cd79da8e83b3
Igor Pocta <igor.pocta@witte-automotive.cz>
2017-07-21 16:24:23 +0200

    xxx

--- src/ambulance-connector/DI/Extension.php
+++ src/ambulance-connector/DI/Extension.php
@@ -36,7 +36,7 @@ class Extension extends CompilerExtension
 				$config['token'],
 				$config['location'],
 				$config['uri'],
-			])->;
+			]);
 
 		$builder->addDefinition($this->prefix('ambulance'))
 			->setClass(Ambulance::class, [
