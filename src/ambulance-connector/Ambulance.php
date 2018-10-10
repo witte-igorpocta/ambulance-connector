d6364c2aa711c63911df81668abd2b5ea7265a2a
Igor Pocta <igor.pocta@witte-automotive.cz>
2018-10-10 09:24:07 +0200

    Typo

--- src/ambulance-connector/Ambulance.php
+++ src/ambulance-connector/Ambulance.php
@@ -20,7 +20,7 @@ class Ambulance extends Gateway implements IGateway
 		1 => "Pondělí",
 		2 => "Úterý",
 		3 => "Středa",
-		4 => "Črvrtek",
+		4 => "Čtvrtek",
 		5 => "Pátek",
 		6 => "Sobota",
 		7 => "Neděle",
